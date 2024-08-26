3139b82b974cc34f484bb8804b7b64f449057765
VoFromDaM <shaunsmart999@gmail.com>
2024-08-26 11:39:33 -0500

    Assignment

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # DSA-Fundamentals
- .
+
+In your repo's readme, describe:
+
+ 
+
+* The Fibonacci Sequence
+
+a sequence where the sum of the previous two numbers make up that
+
+* The difference in the loop and recursion model
+
+loop is instruction level and recursion is function level
+
+* The Big O Notation for this algorithm and how you arrive at that conclusion
+
---
+++ VoDSA/numbers.cs
@@ -0,0 +1,37 @@
+﻿namespace VoDSA;
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("W Assignment btw!");
+    
+    {
+       
+        {
+            int Number1 = 0, Number2 = 1, nextNumber, number;
+            Console.Write("Enter the no. upto which print the Fibonacci series : ");
+            number = int.Parse(Console.ReadLine());
+            
+            Console.Write(Number1 + " " + Number2 + " ");
+            nextNumber = Number1 + Number2;
+            
+            for (int i = 2; nextNumber < number; i++)
+            {
+                Console.Write(nextNumber + " ");
+                Number1 = Number2;
+                Number2 = nextNumber;
+                nextNumber = Number1 + Number2;
+            }
+            
+            Console.ReadKey();
+        }
+    }
+    }}
+    
+
+
+
+
